c847f6111eebe68daadd2b6aa146678335af7d8f
PBocer <przemek.bocer@gmail.com>
2017-08-07 16:51:55 +0200

    Add a task related to the Company (list of employees and adding new ones)

---
+++ src/oop/company/Company.java
@@ -0,0 +1,57 @@
+package oop.company;
+
+/**
+ * Created by Johny on 07.08.2017.
+ */
+public class Company {
+
+    private String name;
+    private Employee[] employees;
+    private int numberOfEmployees;
+
+    public Company() {
+        employees = new Employee[10];
+        numberOfEmployees = 0;
+    }
+
+
+    //napisac metode ktora wyswietli wszystkich pracownikow
+    public void printEmployeesBad() {
+        for (Employee emp : employees) {
+            emp.describeEmployee();
+        }
+    }
+
+    //Poprawna wersja bez bledu
+    public void printEmployees(){
+        if(numberOfEmployees == 0){
+            System.out.println("Brak pracownikow");
+        }else{
+            for(int i = 0; i< numberOfEmployees; i++){
+                Employee emp = employees[i];
+                emp.describeEmployee();
+            }
+        }
+
+    }
+
+    public double countAverageSalary(){
+        //policzyc srednia pensje dla pracownikow
+        return 0;
+    }
+    public double countAverageAge(){
+        //policzyc sredni wiek dla pracownikow
+        return 0;
+    }
+
+    public void getEmployeeWithHigestSalary(){
+        //znalezc pracownika z najwieksza wyplata
+    }
+
+    //Metoda do dodawania pracownika do tablica pracownikow
+    public void addEmployee(Employee nextEmployee) {
+        employees[numberOfEmployees] = nextEmployee;
+        numberOfEmployees++;
+    }
+}
+
---
+++ src/oop/company/Employee.java
@@ -0,0 +1,57 @@
+package oop.company;
+
+/**
+ * Created by Johny on 07.08.2017.
+ */
+public class Employee {
+
+    private String name;
+    private String surname;
+    private int age;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String s) {
+        this.surname = s;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int a) {
+        if (a > 0) {
+            this.age = a;
+        } else {
+            System.out.println("Wiek nie moze byc ujemny");
+        }
+
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 0) {
+            System.out.println("Pensja nie moze byc mniejsza od zero");
+        } else {
+            this.salary = salary;
+        }
+    }
+
+    public void describeEmployee(){
+        System.out.println("Name: " + this.name + " Surname: " + this.surname + " Age: " + this.age);
+    }
+}

fdbf8daa2b29f22b05ca834f90e77086463f8a62
PBocer <przemek.bocer@gmail.com>
2017-08-11 14:49:18 +0200

    Add new tasks related with inheritance

---
+++ src/oop/inheritance/Pracownik.java
@@ -0,0 +1,22 @@
+package oop.inheritance;
+
+public class Pracownik extends Osoba {
+    private double pensja;
+    private String stanowisko;
+
+    public double getPensja() {
+        return pensja;
+    }
+
+    public void setPensja(double pensja) {
+        this.pensja = pensja;
+    }
+
+    public String getStanowisko() {
+        return stanowisko;
+    }
+
+    public void setStanowisko(String stanowisko) {
+        this.stanowisko = stanowisko;
+    }
+}

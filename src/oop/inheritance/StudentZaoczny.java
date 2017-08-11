016b9353ebd59f69600056a6195582760c217b60
PBocer <przemek.bocer@gmail.com>
2017-08-11 18:54:17 +0200

    Add tasks related witch inheritance

---
+++ src/oop/inheritance/StudentZaoczny.java
@@ -0,0 +1,10 @@
+package oop.inheritance;
+
+public class StudentZaoczny extends Student implements Pracujacy {
+
+    @Override
+    public void pracuj() {
+
+    }
+}
+
---
+++ src/oop/inheritance/Studentprogram.java
@@ -0,0 +1,16 @@
+package oop.inheritance;
+
+public class Studentprogram {
+    public static void main(String[] args) {
+        Student studentUAM = new Student();
+        Student studentPP = new Student();
+        Student studentUEP = new Student();
+        Student studentAWF = new Student();
+
+
+        System.out.println(Student.liczbaStudentow);
+        //analogicznie jak
+        System.out.println(Math.pow(2, 3));
+    }
+}
+
